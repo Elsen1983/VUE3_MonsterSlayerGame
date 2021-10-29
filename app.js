66c1589ff4f7bc4590a5c22e536ff3a1d3c0921a
Norbert Korom <norbertkorom@gmail.com>
2021-10-29 16:04:21 +0100

    update 3
    fix all validation for form
    add save and load functionality

--- app.js
+++ app.js
@@ -1,41 +1,81 @@
-
 // validation off all details
-function checkValidation(selectedname, health, speed, attack, minDamage, maxDamage, defense, hasMagic, mana, fire, lightning, heal){
-    console.log(selectedname + " [hp:" + health + "] - [speed:" + speed + "] - [attack:" + attack + "] - [minDmg:" + minDamage +
-                "] - [maxDmg:" + maxDamage + "] - [hasMagic:" + hasMagic +  "] - [mana:" + mana + "] - [fire:" + fire + "] - [lightning:" + lightning +
-                "] - [heal:" + heal + "]");
+function checkValidation(inputObject){
+    let selectedname = inputObject.name, 
+        health = inputObject.health,
+        speed = inputObject.speed,
+        attack = inputObject.attack,
+        minDamage = inputObject.damageMin,
+        maxDamage = inputObject.damageMax,
+        defense = inputObject.defense,
+        hasMagic = inputObject.hasMagic,
+        mana = inputObject.magic.mana,
+        fire = inputObject.magic.fireBallMagic,
+        lightning = inputObject.magic.lightningBoltMagic,
+        heal = inputObject.magic.healingMagic;
+
     if( selectedname!=='' 
-        && (health>=1 && health <=100)
-        && (speed>=1 && speed <=10)
-        && (attack>=25 && attack<=75) 
-        && (minDamage>=1 && minDamage<=6) 
-        && (maxDamage>=7 && maxDamage<=15) 
-        && (defense>=1 && defense<=50)){
+        && (health>=1 && health <=100) && (speed>=1 && speed <=10) && (attack>=25 && attack<=75)  && (minDamage>=1 && minDamage<=6) && (maxDamage>=7 && maxDamage<=15) && (defense>=1 && defense<=50)){
             if(hasMagic === true){
                 const spells = [fire, lightning, heal];
                 const isLearned = (learned) => learned === true;
+                // use .some() to find that any spell is selected (true)
                 if(spells.some(isLearned)){
-                    console.log("found selected spell")
                     if(mana>=1 && mana<=100){
-                        console.log("fully valid")
-                        return true;
+                        console.log("TRUE - fully valid")
+                        logDetails(inputObject);
+                        return "fullValid";
+                        // return true;
                     }else{
-                        console.log("no mana added")
-                        return false;
+                        console.log("FALSE - no mana added")
+                        // return false;
+                        return "manaMissing";
                     }
-                    
                 }else{
-                    console.log("missing spell selection");
-                    return false;
+                    console.log("FALSE - missing spell selection");
+                    // return false;
+                    return "missingSpellSelection";
                 }
             }else{
-                console.log("no magic selected and valid")
-                return true;
+                console.log("TRUE - no magic selected but valid")
+                logDetails(inputObject);
+                // return true;
+                return "fullValid";
             }
     }else{
         console.log("FALSE - invalid details..") 
+        // return false;
+        return "wrongInput";
+    }
+}
+
+// validation for only number
+function isNumber(e) {
+    let char = String.fromCharCode(e.keyCode);
+    if (/^[0-9]+$/.test(char)){
+        //return character when that is a number
+        return true;
+    }else {
+        // e.preventDefault();
         return false;
     }
+}
+
+// validation for only text
+function isString(e) {
+    let char = String.fromCharCode(e.keyCode);
+    if (/^[A-Za-z\s]+$/.test(char)) {
+        //return character when that is a character
+        return true;
+    }else{
+        // e.preventDefault();
+        return false;
+    }
+}
+
+function logDetails(input){
+//     console.log(input.name + " [hp:" + input.health + "] - [speed:" + input.speed + "] - [attack:" + input.attack + "] - [minDmg:" + input.damageMin +
+//                 "] - [maxDmg:" + input.damageMax + "] - [defense:" + input.defense + "] - [hasMagic:" + input.hasMagic +  "] - [mana:" + input.magic.mana + 
+//                 "] - [fire:" + input.magic.fireBallMagic + "] - [lightning:" + input.magic.lightningBoltMagic + "] - [heal:" + input.magic.healingMagic + "]");
 }
 
 function capitalizeName(input){
@@ -70,7 +110,7 @@ function spellSelected(checkboxesName){
     };
 }
 
-// VUE APP CODE
+// ----- VUE APP CODE ------
 const app = Vue.createApp({
     data(){
         return {
@@ -107,81 +147,36 @@ const app = Vue.createApp({
                     healingMagic: false
                 },
             },
-            spellMissing: false,
+            spellMissingMonster: false,
+            spellMissingPlayer: false,
             detailsSaved: false,
             sameName: false,
-            monsterS: false,
-            validForm: false
+            validForm: false,
+            monsterErrorMessage : '',
+            playerErrorMessage : '',
+            savedDetailsEarlier : this.savedEarlier()
         }
     },
     computed:{
-        //remove selected spells if monster not using magic
-        monsterHasMagicCheck(){
-            if(this.monster.hasMagic === false){
-                this.monster.magic.mana = '';
-                this.monster.magic.fireBallMagic = false;
-                this.monster.magic.lightningBoltMagic = false;
-                this.monster.magic.healingMagic = false;
-                return false;
-            }
-        },
-        //remove selected spells if monster/player not using magic
-        playerHasMagicCheck(){
-            if(this.player.hasMagic === false){
-                this.player.magic.mana = '';
-                this.player.magic.fireBallMagic = false;
-                this.player.magic.lightningBoltMagic = false;
-                this.player.magic.healingMagic = false;
-                return false;
-            }
-        },
-
-        // @click="isRequiredMonsterSpell"
-        // :required="isRequiredMonsterSpell"
-
-        // isRequiredMonsterSpell(){
-        //     if(!this.monster.magic.fireBallMagic 
-        //         && !this.monster.magic.lightningBoltMagic
-        //         && !this.monster.magic.healingMagic){
-        //             return spellSelected("monsterSpell");
-        //         }else{
-        //             return
-        //         }
-                
-        // }
+        savedBefore(){
+            // console.log("savedBefore called");
+            return !this.savedDetailsEarlier;
+        }
     },
     watch:{
-
+    
     },
     methods: {
-        // validation for only number
-        isNumber(e) {
-            let char = String.fromCharCode(e.keyCode);
-            if (/^[0-9]+$/.test(char)){
-                return true;
-            }else {
-                e.preventDefault();
-            }
-        },
-        // validation for only number
-        isString(e) {
-            let char = String.fromCharCode(e.keyCode);
-            if (/^[A-Za-z\s]+$/.test(char)) {
-                return true;
-            }else{
-                e.preventDefault();
-            }
-        },
+        
         // compare the monster's and player's name
         checkNamesConflict(){
-            // console.log("monster: " + this.monster.name + ", player: " + this.player.name)
+            // if none of name field is empty
             if(this.monster.name !== '' && this.player.name !== ''){
+                // same name for Monster and Player
                 if(this.monster.name === this.player.name){
-                    // console.log("same name")
                     this.validForm = false;
                     this.sameName = true;
                 }else{
-                    // console.log("diff name")
                     this.validForm = true;
                     this.sameName = false;
                 }
@@ -189,34 +184,105 @@ const app = Vue.createApp({
                 this.sameName = false;
             }
         },
-        
+        clearSpells(input){
+            if(input === "monster"){
+                if(this.monster.hasMagic === false){
+                    this.monster.magic.mana = '';
+                    this.monster.magic.fireBallMagic = false;
+                    this.monster.magic.lightningBoltMagic = false;
+                    this.monster.magic.healingMagic = false;
+                    this.spellMissingMonster = false;
+                }
+            }else{
+                if(this.player.hasMagic === false){
+                    this.player.magic.mana = '';
+                    this.player.magic.fireBallMagic = false;
+                    this.player.magic.lightningBoltMagic = false;
+                    this.player.magic.healingMagic = false;
+                    this.spellMissingPlayer = false;
+                }
+            }
+        },
+        savedEarlier(){
+            if(localStorage.getItem('monsterDetails') !== null && localStorage.getItem('playerDetails') !== null){
+                return true;
+            }else{
+                return false;
+            }
+        },
         saveDetails(){
-
+            const validationForSave = this.validateDetails();
+            if(validationForSave){
+                console.log("Save the details ...");
+                this.monsterErrorMessage = '';
+                this.playerErrorMessage = '';
+                this.spellMissingMonster = false;
+                this.spellMissingPlayer = false;
+                localStorage.setItem('monsterDetails', JSON.stringify(this.monster));
+                localStorage.setItem('playerDetails', JSON.stringify(this.player));
+                this.savedDetailsEarlier = true;
+                alert("Details are saved!");
+            }
         },
         loadDetails(){
+            if(this.savedDetailsEarlier){
+                let monster = JSON.parse(localStorage.getItem('monsterDetails'));
+                let player = JSON.parse(localStorage.getItem('playerDetails'));
+                this.monster = monster;
+                this.player = player;
+                console.log(monster);
+                console.log(player);
+            }
+        },
+        validateDetails(){
+            // console.log('validation called');
+            let monsterCheck = checkValidation(this.monster);
+            let playerCheck = checkValidation(this.player);
+            this.checkNamesConflict();
+            if( monsterCheck === "fullValid" && playerCheck === "fullValid" && this.validForm){
+                return true;
+            }else{
+                switch(monsterCheck){
+                    case 'missingSpellSelection':
+                        this.spellMissingMonster = true;
+                        break;
+                    case 'manaMissing':
+                        this.monsterErrorMessage = "Mana is necessary for spells!"; 
+                        break;
+                    case 'wrongInput': 
+                        this.monsterErrorMessage = "One or more details are invalid!"; 
+                        break;
+                    default:
+                        this.monsterErrorMessage = '';
+                        break;
+                }
+                switch(playerCheck){
+                    case 'missingSpellSelection':
+                        this.spellMissingPlayer = true;
+                        break;
+                    case 'manaMissing':
+                        this.playerErrorMessage = "Mana is necessary for spells!"; 
+                        break;
+                    case 'wrongInput': 
+                        this.playerErrorMessage = "One or more details are invalid!"; 
+                        break;
+                    default:
+                        this.playerErrorMessage = '';
+                        break;
+                }
 
+                return false;                
+            }
         },
         submitDetails(){
-            console.log('submitdetails called')
-            if(checkValidation( this.monster.name, this.monster.health, this.monster.speed, this.monster.attack, 
-                                this.monster.damageMin, this.monster.damageMax, this.monster.defense, this.monster.hasMagic, this.monster.magic.mana,
-                                this.monster.magic.fireBallMagic, this.monster.magic.lightningBoltMagic, this.monster.magic.healingMagic) 
-                && checkValidation( this.player.name, this.player.health, this.player.speed, this.player.attack, 
-                                this.player.damageMin, this.player.damageMax, this.player.defense, this.player.hasMagic, this.player.magic.mana,
-                                this.player.magic.fireBallMagic, this.player.magic.lightningBoltMagic, this.player.magic.healingMagic)
-                && this.validForm){
-                    this.monster.name = capitalizeName(this.monster.name);
-                    this.player.name = capitalizeName(this.player.name);
-                    this.detailsSaved = true;
-            }else{
-                console.log("what the fuck ...")
-                console.log(checkValidation( this.monster.name, this.monster.health, this.monster.speed, this.monster.attack, 
-                    this.monster.damageMin, this.monster.damageMax, this.monster.defense, this.monster.hasMagic, this.monster.magic.mana,
-                    this.monster.magic.fireBallMagic, this.monster.magic.lightningBoltMagic, this.monster.magic.healingMagic)),
-                console.log(checkValidation( this.player.name, this.player.health, this.player.speed, this.player.attack, 
-                    this.player.damageMin, this.player.damageMax, this.player.defense, this.player.hasMagic, this.player.magic.mana,
-                    this.player.magic.fireBallMagic, this.player.magic.lightningBoltMagic, this.player.magic.healingMagic));
-                console.log(this.validForm)
+            this.monsterErrorMessage = '';
+            this.playerErrorMessage = '';
+            const validOrNot = this.validateDetails();
+            // console.log("Validation is " + validOrNot);
+            if(validOrNot){
+                this.monster.name = capitalizeName(this.monster.name);
+                this.player.name = capitalizeName(this.player.name);
+                this.detailsSaved = true;
             }
         }
     }
